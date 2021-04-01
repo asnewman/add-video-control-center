d1611ce479dd5cf86d1e2e82f1221f110274b1da
asnewman <ashznewman@gmail.com>
2021-03-31 18:12:44 -0700

    add email options

--- src/pages/QRGenerator.js
+++ src/pages/QRGenerator.js
@@ -11,6 +11,7 @@ const Field = styled.div`
   label {
     width: 200px;
     display: inline-block;
+    margin-bottom: 10px;
   }
 `;
 
@@ -18,19 +19,23 @@ function QRGenerator () {
   const [deviceCode, setDeviceCode] = useState('');
   const [ssid, setSsid] = useState('');
   const [wifiPass, setWifiPass] = useState('');
+  const [sendEmail, setSendEmail] = useState(false);
+  const [email, setEmail] = useState('');
 
   function generateQRCode (e) {
     e.preventDefault();
 
-    if (!deviceCode || !ssid || !wifiPass) {
-      window.alert('Please fill out all of the fields');
-      return;
-    }
+    // if (!deviceCode || !ssid || !wifiPass) {
+    //   window.alert('Please fill out all of the fields');
+    //   return;
+    // }
 
     QRCode.toCanvas(document.getElementById('qrcodecanvas'), JSON.stringify({
       deviceCode,
       ssid,
-      password: wifiPass
+      password: wifiPass,
+      "SEND_EMAIL": sendEmail,
+      "RECEPIENT_EMAIL": email
     }), { width: 500 }, function (err) {
       if (err) window.alert('Failed to create QR Code');
       else console.log('QRCode created successfully');
@@ -52,6 +57,14 @@ function QRGenerator () {
       <Field>
         <label htmlFor='wifipass'>WiFi Password</label>
         <input type='password' id='wifipass' name='wifi password' value={wifiPass} onChange={(e) => setWifiPass(e.target.value)} />
+      </Field>
+      <Field>
+        <label htmlFor='sendemail'>Send Email on Activation</label>
+        <input type='checkbox' id='sendemail' name='sendemail' value={sendEmail} onChange={(e) => setSendEmail(e.target.checked)} />
+      </Field>
+      <Field>
+        <label htmlFor='email'>Email</label>
+        <input type='text' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
       </Field>
       <GenerateButton onClick={generateQRCode}>Generate</GenerateButton>
       <canvas id='qrcodecanvas' />
